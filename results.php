5ec32750fb9980e79067d9654d193ed6d4c9e607
omrymar <olegrim2525@ukr.net>
2016-04-17 13:29:37 +0300

    Update results.php

--- results.php
+++ results.php
@@ -13,6 +13,16 @@ function ret_res_big_min($dice_type,$min){
 /*
  Варіант, коли кількість рядків у яких виводяться змінні зберігається
  */
+ function ret_res_big_min_1($dice_type,$min,$i){
+    $change = rand(1,$dice_type);
+    if ($change >= $min){
+        echo " $i. "." number = "." $change"."<br/>";
+    }
+    else{
+        echo "$i."." "."<br/>";
+    }
+}
+
 function ret_res_big_min_2($dice_type,$min,$i){
   $change = rand(1,$dice_type);
   if ($change >= $min){
