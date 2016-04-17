7abb2403ebdf97cc5ab77c8a42f741158b11e299
omrymar <olegrim2525@ukr.net>
2016-04-17 13:30:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $count = isset($_GET['count']) ? $_GET['count'] : 1;
 $min = isset($_GET['min']) ? $_GET['min'] : 3;
 
  for ($i = 0; $i < $count; $i++){
-   echo ret_res_big_min_2($dice_type,$min,$i);
+   //echo ret_res_big_min_2($dice_type,$min,$i);
+   echo ret_res_big_min_1($dice_type,$min,$i);
    //echo ret_res_big_min($dice_type,$min);
  }
